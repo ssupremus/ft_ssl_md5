257e54ee4a68f0ab33dbb7728f2cdee25951d777
Yurii Sushkov <s.supremus@gmail.com>
2019-09-29 12:04:46 +0300

    flags handling

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -3,4 +3,17 @@
 
 # include "../libft/libft.h"
 
+typedef struct    s_flag
+{
+  int             p;
+  int             q;
+  int             r;
+  int             s;
+}                 t_flag;
+
+typedef struct    s_ssl
+{
+  t_flag          flags;
+}                 t_ssl;
+
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -19,21 +19,52 @@ void	error(char *arg)
 	exit(-1);
 }
 
-void	processing(int length, char **args)
+void	flags(t_ssl *ssl, int length, char **args)
 {
-	int i;
+	int	i;
 
-	i = 1;
-	while (i < length)
-		ft_putendl(args[i++]);
+	i = 2;
+	while (i < length && args[i][0] == '-' && ft_strlen(args[i]) == 2)
+	{
+		if (args[i][1] == 'p')
+			ssl->flags.p = 1;
+		else if (args[i][1] == 'q')
+			ssl->flags.q = 1;
+		else if (args[i][1] == 'r')
+			ssl->flags.r = 1;
+		else if (args[i][1] == 's')
+			ssl->flags.s = 1;
+		else
+		{
+			ft_putendl("WRONG FLAG");
+			exit(-1);
+		}
+		i++;
+	}
+}
+
+void	processing(t_ssl *ssl, int length, char **args)
+{
+	flags(ssl, length, args);
+}
+
+void	initialize(t_ssl *ssl)
+{
+	ssl->flags.p = 0;
+	ssl->flags.q = 0;
+	ssl->flags.r = 0;
+	ssl->flags.s = 0;
 }
 
 int	main(int argc, char **argv)
 {
+	t_ssl	ssl;
+
+	initialize(&ssl);
 	if (argc == 1)
 		usage();
 	else if (ft_strcmp(argv[1], "md5") == 0 || ft_strcmp(argv[1], "sha256") == 0)
-		processing(argc, argv);
+		processing(&ssl, argc, argv);
 	else
 		error(argv[1]);
 	return (0);
